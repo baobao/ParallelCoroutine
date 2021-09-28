dbeadfd67d155f5564c63395c8dfe64717b6fbb1
ohbashunsuke <shunsuke.ohba@gmail.com>
2021-09-29 08:27:51 +0900

    Change StopCoroutine

--- Runtime/ParallelCoroutine.cs
+++ Runtime/ParallelCoroutine.cs
@@ -7,12 +7,11 @@ namespace info.shibuya24
 {
     public static class ParallelCoroutine
     {
-        public static (Coroutine mainCoroutine, List<Coroutine> childCoroutineList) 
-            Execute(MonoBehaviour owner, IList<IEnumerator> ienumeratorList, Action complete = null)
+        public static ParallelCoroutineResult Execute(MonoBehaviour owner, IList<IEnumerator> ienumeratorList, Action complete = null)
         {
             List<Coroutine> childCoroutineList = null;
             var mainCoroutine = owner.StartCoroutine(InternalExecute(owner, ienumeratorList, complete, x => childCoroutineList = x));
-            return (mainCoroutine, childCoroutineList);
+            return new ParallelCoroutineResult(owner, mainCoroutine, childCoroutineList);
         }
 
         private static IEnumerator InternalExecute(MonoBehaviour owner, 
@@ -32,6 +31,32 @@ namespace info.shibuya24
             }
 
             complete?.Invoke();
+        }
+    }
+
+    public readonly struct ParallelCoroutineResult
+    {
+        private readonly Coroutine _main;
+        private readonly List<Coroutine> _childCoroutineList;
+        private readonly MonoBehaviour _owner;
+        
+        public ParallelCoroutineResult(MonoBehaviour owner, Coroutine main, List<Coroutine> childCoroutineList)
+        {
+            _owner = owner;
+            _main = main;
+            _childCoroutineList = childCoroutineList;
+        }
+
+        public void StopCoroutine()
+        {
+            if (_owner == null) return;
+            _owner.StopCoroutine(_main);
+
+            if (_childCoroutineList != null && _childCoroutineList.Count > 0)
+            {
+                foreach(var col in _childCoroutineList)
+                    _owner.StopCoroutine(col);
+            }
         }
     }
 }
